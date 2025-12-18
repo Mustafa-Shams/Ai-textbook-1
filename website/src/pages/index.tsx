07acc8a4fe6e7bccf30ef2cf349bf3ce1dba23f4
MustafaShams <77402878+Mustafa-Shams@users.noreply.github.com>
2025-12-19 00:48:56 +0500

    .

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/index">
+            to="/docs/">
             Start Learning
           </Link>
         </div>
